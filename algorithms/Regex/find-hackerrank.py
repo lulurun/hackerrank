b346458719250934b2f741aad03d8aef50c66aa4
Nirvam <marvin.beeblebrox@gmail.com>
2013-07-19 19:59:33 +0800

    Solved regex/find-hackerrank; folder reconstruct

---
+++ algorithms/Regex/find-hackerrank.py
@@ -0,0 +1,38 @@
+﻿import re
+
+word=re.compile('hackerrank')
+
+
+def get_input():
+    '''Reads input. Hackerrank style.'''
+    l=input()
+    lines=[]
+    for i in range(int(l)):
+        lines.append(input())
+    return l,lines
+    
+    
+def search_word(line):
+    try:
+        begin,end=list(zip(*[x.span() for x in word.finditer(line)]))
+    except ValueError:
+        return -1
+    b = 0 in begin
+    e = len(line) in end
+    if b and e:
+        return 0
+    elif b:
+        return 1
+    elif e:
+        return 2
+    else:
+        return -1
+    
+    
+def main():
+    line_counts,lines=get_input()
+    for line in lines:
+        print(search_word(line))
+    
+if __name__ == '__main__':
+    main()
