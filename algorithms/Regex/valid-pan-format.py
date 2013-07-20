2d49df5f1640a0d14e5efc0d728027d0ee5bef8a
Nirvam <marvin.beeblebrox@gmail.com>
2013-07-20 20:49:05 +0800

    valid-pan-format solved

---
+++ algorithms/Regex/valid-pan-format.py
@@ -0,0 +1,29 @@
+﻿import re
+
+
+def get_input():
+    '''Reads input. Hackerrank style.'''
+    n=input()
+    lines=[]
+    for i in range(int(n)):
+        lines.append(input())
+    return lines
+    
+    
+def test_pan(line):
+    '''PAN format test (CCCCCDDDDC).'''
+    pt=re.compile('^[A-Z]{5}\d{4}[A-Z]$')
+    return pt.search(line)
+    
+    
+def main():
+    lines=get_input()
+    for l in lines:
+        if test_pan(l):
+            print('YES')
+        else:
+            print('NO')
+        
+    
+if __name__ == '__main__':
+    main()
