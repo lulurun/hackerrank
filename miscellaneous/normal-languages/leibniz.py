42768d1aa1edfeb7dd633dac8159447f7c0f2c43
Nirvam <marvin.beeblebrox@gmail.com>
2013-07-21 22:06:31 +0800

    fizzbuzz & leibuniz (golf)

---
+++ miscellaneous/normal-languages/leibniz.py
@@ -0,0 +1,8 @@
+import itertools
+f=lambda x,y:sum(x,[(-1)**n*x**(2*n+1)/(2*n+y)])
+r=lambda x:itertoos.reduce()
+for x in range(int(input())):c.append(float(input()))
+for x in c:print('%.3f\n%3.f'%f(x,1),f(x,2))
+
+import math
+math.sin()
