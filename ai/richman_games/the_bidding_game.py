98a18e279241f5763823560c241435deb566e9cf
Nirvam <marvin.beeblebrox@gmail.com>
2013-07-21 12:44:49 +0800

    ai/the_bidding_game created

---
+++ ai/richman_games/the_bidding_game.py
@@ -0,0 +1,25 @@
+'''
+Created on Jul 21, 2013
+
+@author: Nirvam
+'''
+
+def get_input():
+    '''Reads input. Hackerrank style.'''
+    l=input()
+    lines=[]
+    for i in range(int(l)):
+        lines.append(input())
+    return l,lines
+
+
+def calculate_bid(player,pos,first_moves,second_moves):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
